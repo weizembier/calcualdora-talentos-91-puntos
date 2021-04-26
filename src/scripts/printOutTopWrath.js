370e8b951a5ab62e4907df531c321a8c734ca006
Francisco Berenguel Alcaraz <47412697+weizembier@users.noreply.github.com>
2021-04-26 21:47:51 +0200

    Update printOutTopWrath.js

--- src/scripts/printOutTopWrath.js
+++ src/scripts/printOutTopWrath.js
@@ -155,15 +155,15 @@ for (var printTreeID = 0; printTreeID < numberOfTrees; printTreeID++) {
 
 						//This has to do with "if the talent has points
 						if (rankTop[i][0] >= 1)
-							massiveReplaceString += '<div id = "iconOverGreen'+ i +'" style = "position: relative; z-index:50;"><div id = "iconOver"><img src = "../src/images/talents/icon-over-green.gif" border = "0" /></div></div>';
+							massiveReplaceString += '<div id = "iconOverGreen'+ i +'" style = "position: relative; z-index:50;"><div id = "iconOver"><img src = "src/images/talents/icon-over-green.gif" border = "0" /></div></div>';
 						else
-							massiveReplaceString += '<div id = "iconOverGreen'+ i +'" style = "visibility:hidden; position: relative; z-index:50;"><div id = "iconOver"><img src = "../src/images/talents/icon-over-green.gif" border = "0"></div></div>';
+							massiveReplaceString += '<div id = "iconOverGreen'+ i +'" style = "visibility:hidden; position: relative; z-index:50;"><div id = "iconOver"><img src = "src/images/talents/icon-over-green.gif" border = "0"></div></div>';
 
 						//This is if the talent has max points
 						if (rankTop[i][0] == talent[i][2])
-							massiveReplaceString += '<div id = "iconOverYellow'+ i +'" style = "position: relative; z-index:50;"><div id = "iconOver"><img src = "../src/images/talents/icon-over-yellow.gif" border = "0"></div></div>';
+							massiveReplaceString += '<div id = "iconOverYellow'+ i +'" style = "position: relative; z-index:50;"><div id = "iconOver"><img src = "src/images/talents/icon-over-yellow.gif" border = "0"></div></div>';
 						else 
-							massiveReplaceString += '<div id = "iconOverYellow'+ i +'" style = "visibility:hidden; position: relative; z-index:50;"><div id = "iconOver"><img src = "../src/images/talents/icon-over-yellow.gif" border = "0"></div></div>';
+							massiveReplaceString += '<div id = "iconOverYellow'+ i +'" style = "visibility:hidden; position: relative; z-index:50;"><div id = "iconOver"><img src = "src/images/talents/icon-over-yellow.gif" border = "0"></div></div>';
 
 						//This is if the talent can't have points..?
 						if (rankTop[i][0] == 0)							
